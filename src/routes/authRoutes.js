ab0e09d4ccc7700bdb02bded34a06769a050f04b
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2023-06-06 15:51:59 +0700

    Done Login and Refresh Token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,5 +2,6 @@ const authController = require('../controllers/authController');
 const router = require('express').Router();
 
 router.post('/signup', authController.signupUser);
+router.post('/login', authController.loginUser)
 
 module.exports = router;
