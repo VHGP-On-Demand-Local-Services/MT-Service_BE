99429089199cd2194ebfa0eb913c21ab20bfe37c
FPTnhutminh00 <nhutminh.fpt@gmail.com>
2023-06-10 22:36:36 +0700

    done update service

--- src/controllers/serviceController.js
+++ src/controllers/serviceController.js
@@ -58,6 +58,47 @@ const serviceController = {
         } catch (err) {
             res.status(500).json({ message: 'Server Error !' })
         }
+    },
+    updateService: async(req, res) => {
+        try {
+            const { icon_name, name, expected_price } = req.body
+
+            function validateName(name) {
+                const re = /^[a-zA-Z ]*$/;
+                return re.test(name);
+            }
+
+            if (!validateName(name)) {
+                return res.status(400).json({ message: 'Invalid name!' });
+            }
+            if (!/^\d+$/.test(expected_price)) {
+                return res.status(400).json({ message: 'Expected price can only contain numbers!' });
+            }
+
+            if (!name || !icon_name || !expected_price) {
+                return res.status(400).json({ message: 'Please provide all required information!' });
+            }
+            const existService = await Service.findOne({
+                name: req.body.name
+            });
+            if (existService && existService._id.toString() !== req.params.id) {
+                return res.status(400).json({ message: 'Service alredy Existing !' })
+            }
+
+            const updateService = await Service.findByIdAndUpdate(req.params.id, {
+                icon_name: icon_name,
+                name: name,
+                expected_price: expected_price
+            })
+            if (updateService) {
+                const updateService = await Service.findById(req.params.id);
+                return res.status(200).json(updateService);
+            } else {
+                return res.status(401).json({ message: 'User does not exist!' });
+            }
+        } catch (err) {
+            res.status(500).json({ message: 'Server Error !' })
+        }
     }
 };
 
